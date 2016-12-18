a528c4e595816efa3e4dca12275d3f460754c59a
Pablo Hurtado <hurtado.pani@gmail.com>
2016-12-18 01:44:25 -0300

    FIXED ALL

--- circlet/sc.py
+++ circlet/sc.py
@@ -2,11 +2,10 @@
 
 import datetime
 
-import pytz
-import six
 
 def say(what):
     return "Hey, listen: {what}.".format(what=what)
+
 
 if __name__ == '__main__':
     print datetime.datetime.now()
